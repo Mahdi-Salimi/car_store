64abce8464a0123e166b9837330d5d4046c7fee2
Mahdi-S97 <mahdisalimi.dev@gmail.com>
2024-08-28 13:49:41 +0330

    feat: add buyer and seller groups and register simple admins for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from accounts.models import CustomUser, SellerUserProfile, BuyerUserProfile
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(SellerUserProfile)
+admin.site.register(BuyerUserProfile)
--- accounts/signals.py
+++ accounts/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import Group
+
 
 from accounts.models import CustomUser, BuyerUserProfile, SellerUserProfile
 
@@ -8,12 +10,11 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         if instance.user_type == CustomUser.UserType.BUYER:
             BuyerUserProfile.objects.create(user=instance)
+            group, _ = Group.objects.get_or_create(name='buyer')
+            if group not in instance.groups.all():
+                instance.groups.add(group)
         elif instance.user_type == CustomUser.UserType.SELLER:
             SellerUserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=CustomUser)
-def save_user_profile(sender, instance, **kwargs):
-    if instance.user_type == CustomUser.UserType.BUYER:
-        instance.buyeruserprofile.save()
-    elif instance.user_type == CustomUser.UserType.SELLER:
-        instance.selleruserprofile.save()
+            group, _ = Group.objects.get_or_create(name='seller')
+            if group not in instance.groups.all():
+                instance.groups.add(group)
