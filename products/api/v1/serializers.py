65aac2f275b167b81c98ece6e4cd0e50345de3c3
Mahdi-S97 <mahdisalimi.dev@gmail.com>
2024-09-03 14:57:25 +0330

    feat: add serializers and views and urls for product apps

--- products/api/v1/serializers.py
+++ products/api/v1/serializers.py
@@ -1,23 +1,23 @@
-# from rest_framework import serializers
-# from products.models import Car, CarImage, Wishlist
-#
-# class CarImageSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = CarImage
-#         fields = ['id', 'image_url']
-#
-# class CarSerializer(serializers.ModelSerializer):
-#     images = CarImageSerializer(many=True, read_only=True)
-#
-#     class Meta:
-#         model = Car
-#         fields = ['id', 'seller', 'url', 'title', 'time', 'year', 'mileage', 'location', 'description', 'images', 'created_at', 'price']
-#         read_only_fields = ['seller', 'created_at']
-#
-# class WishlistSerializer(serializers.ModelSerializer):
-#     car = CarSerializer(read_only=True)
-#
-#     class Meta:
-#         model = Wishlist
-#         fields = ['id', 'user', 'car', 'added_at']
-#         read_only_fields = ['user', 'added_at']
+from rest_framework import serializers
+from products.models import Car, CarImage, Wishlist
+
+class CarImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarImage
+        fields = ['id', 'image_url']
+
+class CarSerializer(serializers.ModelSerializer):
+    images = CarImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Car
+        fields = ['id', 'seller', 'url', 'title', 'time', 'year', 'mileage', 'location', 'description', 'images', 'created_at', 'price']
+        read_only_fields = ['seller', 'created_at']
+
+class WishlistSerializer(serializers.ModelSerializer):
+    car = CarSerializer(read_only=True)
+
+    class Meta:
+        model = Wishlist
+        fields = ['id', 'user', 'car', 'added_at']
+        read_only_fields = ['user', 'added_at']
